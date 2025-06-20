53609917dac2c55e7f98e799e0610c566a756786
Luiz Agostinho <167261115+luizagostinho@users.noreply.github.com>
2025-06-19 21:27:50 -0300

    Atualização de exercicios

--- Exercicios/Python/index.py
+++ Exercicios/Python/index.py
@@ -1,6 +1,7 @@
-# Exemplo de f-string em Python
 
-n = 6.0
-a = 'algebra'
-s1 = f'Sua nota final é {n} na disciplina de Python e {a}'
-print(s1)
+
+# Exercicio 
+
+n1 = int(input('Digite um numero:'))
+n2 = int(input('Digite outro numero:'))
+print(f'A soma de {n1} e {n2} é igual a {n1 + n2}')
