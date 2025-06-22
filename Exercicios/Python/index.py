77016db902bcaf78e5d1a9705ef6c113fea9eca3
Luiz Agostinho <167261115+luizagostinho@users.noreply.github.com>
2025-06-22 10:16:25 -0300

    Add f-string and input examples in Python script
    Introduced examples demonstrating the use of f-strings and input in Python. Also improved code structure and comments for clarity.

--- Exercicios/Python/index.py
+++ Exercicios/Python/index.py
@@ -1,6 +1,17 @@
+# Exemplo de f-string em Python
+
+n = 6.0
+a = 'algebra'
+s1 = f'Sua nota final é {n} na disciplina de Python e {a}'
+print(s1)
 
 
-# Exercicio 
+# Exemplo de input em Python
+
+nome = input('Qual o seu nome?')
+print(f'Muito prazer, {nome}!')
+
+# Exercicio: Soma de dois números
 
 n1 = int(input('Digite um numero:'))
 n2 = int(input('Digite outro numero:'))
