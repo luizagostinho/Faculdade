cd5ec1f9c71e4a28cb59b24360fe506ea4e125af
Luiz Agostinho <167261115+luizagostinho@users.noreply.github.com>
2026-05-25 22:07:20 -0300

    adicionando os movimentos y e x da nave do player

--- MountainShooter/code/player.py
+++ MountainShooter/code/player.py
@@ -2,15 +2,27 @@
 # -*- coding: utf-8 -*-
 
 from code.entity import Entity
+import pygame
 
 
 class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
 
-
-    def update(self):
-        pass
-
     def move(self, ):
-        pass
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_UP] and self.rect.top > 0:
+            self.rect.centery -= 2
+
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_DOWN] and self.rect.bottom > 0:
+            self.rect.centery -= -2
+
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.centerx -= 2
+
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_RIGHT] and self.rect.right > 0:
+            self.rect.centerx -= -2
+
